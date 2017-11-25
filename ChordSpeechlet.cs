427e36505bd6cba012d52557b481eeb20ef7d8c4
Adrian Banks <github@adrianbanks.co.uk>
2017-11-25 22:20:34 +0000

    Initial commit

---
+++ ChordSpeechlet.cs
@@ -0,0 +1,49 @@
+﻿using AlexaSkillsKit.Speechlet;
+using AlexaSkillsKit.UI;
+
+namespace Chords
+{
+    public class ChordSpeechlet : Speechlet
+    {
+        public override SpeechletResponse OnLaunch(LaunchRequest launchRequest, Session session)
+        {
+            return BuildSpeechletResponse("Welcome", "Welcome to the Alexa piano chords skill. Please say a chord name.", false);
+        }
+
+        public override void OnSessionStarted(SessionStartedRequest sessionStartedRequest, Session session)
+        {
+        }
+
+        public override SpeechletResponse OnIntent(IntentRequest request, Session session)
+        {
+            var intent = request.Intent;
+            var intentName = intent?.Name;
+
+            if ("ChordIntent".Equals(intentName))
+            {
+                var chord = intent.Slots["chord"];
+                return BuildSpeechletResponse("Chord", "You said " + chord, false);
+            }
+
+            throw new SpeechletException("Invalid Intent");
+        }
+
+        public override void OnSessionEnded(SessionEndedRequest sessionEndedRequest, Session session)
+        {
+        }
+
+        private SpeechletResponse BuildSpeechletResponse(string title, string output, bool shouldEndSession)
+        {
+            return new SpeechletResponse
+            {
+                Card = new SimpleCard
+                {
+                    Title = $"SessionSpeechlet - {title}",
+                    Content = $"SessionSpeechlet - {output}"
+                },
+                OutputSpeech = new PlainTextOutputSpeech { Text = output },
+                ShouldEndSession = shouldEndSession
+            };
+        }
+    }
+}
---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace Chords.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            var speechlet = new ChordSpeechlet();
+            return speechlet.GetResponse(Request);
+        }
+    }
+}
