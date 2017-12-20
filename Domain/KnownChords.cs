29eebb4c88a6ac94bc9c10fb4b3153674b166bb6
Adrian Banks <github@adrianbanks.co.uk>
2017-12-20 22:23:17 +0000

    Add minor seventh chords

--- Domain/KnownChords.cs
+++ Domain/KnownChords.cs
@@ -5,13 +5,17 @@ namespace Chords.Domain
         public static ChordName Major = new ChordName(
             (ChordNames) "" | "major", 
             Positions.First, Positions.Third, Positions.Fifth);
+
+        public static ChordName Major7 = new ChordName(
+            (ChordNames) "major 7" | "major 7th", 
+            Positions.First, Positions.Third, Positions.Fifth, Positions.Seventh);
         
         public static ChordName Minor = new ChordName(
             "minor", 
             Positions.First, Positions.MinorThird, Positions.Fifth);
-        
-        public static ChordName Major7 = new ChordName(
-            (ChordNames) "major 7" | "major 7th", 
-            Positions.First, Positions.Third, Positions.Fifth, Positions.Seventh);
+
+        public static ChordName Minor7 = new ChordName(
+            (ChordNames) "minor 7" | "minor 7th", 
+            Positions.First, Positions.MinorThird, Positions.Fifth, Positions.Seventh);
     }
 }
