388b907e9f8ebe177ba9ce53b81bc2706c2c864d
Adrian Banks <github@adrianbanks.co.uk>
2017-12-23 21:19:13 +0000

    Add more note aliases

--- Domain/Positions.cs
+++ Domain/Positions.cs
@@ -4,43 +4,56 @@ namespace Chords.Domain
     {
         // https://en.wikipedia.org/wiki/Interval_(music)
         
+        Root = 0,
         Unison = 0,
         DiminishedSecond = 0,
         
         MinorSecond = 1,
         AugmentedUnison = 1,
         
+        Second = 2,
         MajorSecond = 2,
         DiminishedThird= 2,
         
         MinorThird = 3,
         AugmentedSecond = 3,
         
+        Third = 4,
         MajorThird = 4,
         DiminishedFourth = 4,
 
+        Fourth = 5,
         PerfectFourth = 5,
         AugmentedThird = 5,
         
         DiminishedFifth = 6,
         AugmentedFourth = 6,
         
+        Fifth = 7,
         PerfectFifth = 7,
         DimishedSixth = 7,
         
         MinorSixth = 8,
         AugmentedFifth = 8,
         
+        Sixth = 9,
         MajorSixth = 9,
         DiminishedSeventh = 9,
         
         MinorSeventh = 10,
         AugmentedSixth = 10,
         
+        Seventh,
         MajorSeventh = 11,
         DiminishedOctave = 11,
         
         PerfectOctave = 12,
-        AugmentedSeventh = 12
+        AugmentedSeventh = 12,
+        
+        Ninth = 2,
+        
+        Eleventh = 5,
+        
+        Thirteenth = 9
     }
 }
