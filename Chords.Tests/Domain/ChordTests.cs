56269a09cef8e73bac95a53eaec200953610964d
Adrian Banks <github@adrianbanks.co.uk>
2017-12-23 23:46:42 +0000

    Add a test for working out the correct chord name

---
+++ Chords.Tests/Domain/ChordTests.cs
@@ -0,0 +1,23 @@
+﻿using Chords.Domain;
+using NUnit.Framework;
+
+namespace Chords.Tests.Domain
+{
+    [TestFixture]
+    public sealed class ChordTests
+    {
+        [TestCase(Note.AFlat, "test chord", "A flat test chord")]
+        [TestCase(Note.C, "another", "C another")]
+        [TestCase(Note.F, "and another", "F and another")]
+        [TestCase(Note.FSharp, "and yet another", "F sharp and yet another")]
+        public void NameIsWorkedOutFromTheNoteAndTheShape(Note note, string chordName, string expectedChordName)
+        {
+            var chordShape = new ChordShape(chordName);
+            var chord = new Chord(note, chordShape, new Note[0]);
+
+            var name = chord.Name;
+            
+            Assert.That(name, Is.EqualTo(expectedChordName));
+        }
+    }
+}
