5977ecb11d68794a65dfaa576d3b2fc1812d8d9a
Adrian Banks <github@adrianbanks.co.uk>
2017-12-23 23:20:25 +0000

    Add a test project

--- Chords.Tests/Properties/AssemblyInfo.cs
+++ Chords.Tests/Properties/AssemblyInfo.cs
@@ -1,12 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Alexa piano chords")]
-[assembly: AssemblyDescription("An Alexa skill for piano chords")]
+[assembly: AssemblyTitle("Alexa piano chords tests")]
 [assembly: AssemblyCopyright("Copyright ©2017 Adrian Banks")]
 [assembly: AssemblyCompany("https://www.adrianbanks.co.uk")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Chords.Tests")]
