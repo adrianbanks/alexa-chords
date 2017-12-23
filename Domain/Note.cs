4764904dbcc75e9847ec7915376a1b9cdd74a01d
Adrian Banks <github@adrianbanks.co.uk>
2017-12-23 22:53:21 +0000

    Add the missing note names back in
    These are required for parsing the correct note. To keep the preferred
    note for output, the aliases have been added after the preferred name.

--- Domain/Note.cs
+++ Domain/Note.cs
@@ -3,16 +3,21 @@ namespace Chords.Domain
     internal enum Note
     {
         A = 0,
-        BFlat = 1, 
+        BFlat = 1,
+        ASharp = 1,
         B = 2,
         C = 3,
         CSharp = 4,
+        DFlat = 4,
         D = 5,
         EFlat = 6,
+        DSharp = 6,
         E = 7,
         F = 8,
         FSharp = 9,
+        GFlat = 9,
         G = 10,
-        AFlat = 11
+        AFlat = 11,
+        GSharp = 11
     }
 }
