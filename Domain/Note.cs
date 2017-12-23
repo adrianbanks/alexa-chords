1544affcd66f8413c84d3b76d558cd59715c3dd2
Adrian Banks <github@adrianbanks.co.uk>
2017-12-23 21:53:01 +0000

    Use the more common names for notes

--- Domain/Note.cs
+++ Domain/Note.cs
@@ -3,21 +3,16 @@ namespace Chords.Domain
     internal enum Note
     {
         A = 0,
-        ASharp = 1, 
         BFlat = 1, 
         B = 2,
         C = 3,
         CSharp = 4,
-        DFlat = 4,
         D = 5,
-        DSharp = 6,
         EFlat = 6,
         E = 7,
         F = 8,
         FSharp = 9,
-        GFlat = 9,
         G = 10,
-        GSharp = 11,
         AFlat = 11
     }
 }
