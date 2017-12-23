7b53f13ef563955106fe3b15d1aee1efc7c5bb5e
Adrian Banks <github@adrianbanks.co.uk>
2017-12-23 23:19:16 +0000

    Tidy up assembly info

--- Chords/Properties/AssemblyInfo.cs
+++ Chords/Properties/AssemblyInfo.cs
@@ -1,35 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Chords")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chords")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Alexa piano chords")]
+[assembly: AssemblyDescription("An Alexa skill for piano chords")]
+[assembly: AssemblyCopyright("Copyright ©2017 Adrian Banks")]
+[assembly: AssemblyCompany("https://www.adrianbanks.co.uk")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9248baf6-52d1-426b-9bd9-009edfce0b97")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
