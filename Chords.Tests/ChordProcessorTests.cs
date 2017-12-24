ad320d41a45fd53cf75f1652f4e1e219a1b2f72f
Adrian Banks <github@adrianbanks.co.uk>
2017-12-24 01:29:36 +0000

    Add more tests for chord processing

--- Chords.Tests/ChordProcessorTests.cs
+++ Chords.Tests/ChordProcessorTests.cs
@@ -1,4 +1,6 @@
-﻿using AlexaSkillsKit.Speechlet;
+﻿using System;
+using System.Text.RegularExpressions;
+using AlexaSkillsKit.Speechlet;
 using AlexaSkillsKit.UI;
 using Chords.Speech;
 using NUnit.Framework;
@@ -32,12 +34,59 @@ namespace Chords.Tests
             AssertIsPlainTextResponse(response, expectedMessage);
         }
 
+        [Test]
+        public void OutputIsSsml_WhenTheChordIsRecognised()
+        {
+            var processor = new ChordProcessor(new Logger(), new ChordFinder());
+
+            var response = processor.ProcessChord("C sharp major");
+
+            Assert.That(response.OutputSpeech, Is.TypeOf<SsmlOutputSpeech>());
+        }
+
+        [Test]
+        public void OutputContainsTheNoteName_WhenTheChordIsRecognised()
+        {
+            var processor = new ChordProcessor(new Logger(), new ChordFinder());
+
+            var response = processor.ProcessChord("C sharp minor");
+
+            var output = (SsmlOutputSpeech) response.OutputSpeech;
+            Assert.That(output.Ssml, Does.Contain("C sharp minor"));
+        }
+
+        [Test]
+        public void OutputContainsTheChordNotesName_WhenTheChordIsRecognised()
+        {
+            var processor = new ChordProcessor(new Logger(), new ChordFinder());
+
+            var response = processor.ProcessChord("C sharp major");
+
+            AssertSsmlContentContainsText(response, "C sharp");
+            AssertSsmlContentContainsText(response, "F");
+            AssertSsmlContentContainsText(response, "A flat");
+        }
+
         private void AssertIsPlainTextResponse(SpeechletResponse response, string expectedText)
         {
             Assert.That(response.OutputSpeech, Is.TypeOf<PlainTextOutputSpeech>());
             
             var output = (PlainTextOutputSpeech) response.OutputSpeech;
             Assert.That(output.Text, Is.EqualTo(expectedText));
+        }
+
+        private void AssertSsmlContentContainsText(SpeechletResponse response, string expectedText)
+        {
+            Assert.That(response.OutputSpeech, Is.TypeOf<SsmlOutputSpeech>());
+
+            var output = (SsmlOutputSpeech) response.OutputSpeech;
+            var outputText = StripXmlTags(output.Ssml);
+            Assert.That(outputText, Does.Contain(expectedText));
+
+            string StripXmlTags(string input)
+            {
+                return Regex.Replace(input, "<.*?>", string.Empty);
+            }
         }
     }
 }
