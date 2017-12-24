0ba42cf88aae5ff77116ae14e7f93f8dd00db0e4
Adrian Banks <github@adrianbanks.co.uk>
2017-12-24 00:15:52 +0000

    Move the core logic out of the speechlet

---
+++ Chords/ChordProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using AlexaSkillsKit.Slu;
+using AlexaSkillsKit.Speechlet;
+using Chords.Speech;
+
+namespace Chords
+{
+    internal sealed class ChordProcessor
+    {
+        public SpeechletResponse ProcessChord(Intent intent)
+        {
+            var chordName = intent.Slots["chord"];
+            Trace.WriteLine($"Chord was: {chordName.Value}");
+
+            if (string.IsNullOrEmpty(chordName.Value))
+            {
+                return PlainTextResponseFactory.Create("Didn't recognise that chord", false);
+            }
+            
+            try
+            {
+                return ProcessChord(chordName.Value);
+            }
+            catch (ChordNotFoundException exception)
+            {
+                var chord = string.Join(" ", exception.Words);
+                return PlainTextResponseFactory.Create($"Didn't recognise chord {chord}", false);
+            }
+        }
+        
+        private static SpeechletResponse ProcessChord(string chordName)
+        {
+            var chord = new ChordFinder().GetChord(chordName);
+
+            Trace.WriteLine($"Notes are : {string.Join(", ", chord.Notes)}");
+
+            var ssml = $"Notes in {chord.Name} <break strength='medium'/> are <break strength='medium'/> {chord.ToNotesSsml()}";
+            return SsmlResponseFactory.Create(ssml, false);
+        }
+    }
+}
--- Chords/ChordSpeechlet.cs
+++ Chords/ChordSpeechlet.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using AlexaSkillsKit.Slu;
 using AlexaSkillsKit.Speechlet;
 using Chords.Speech;
 
@@ -32,7 +31,7 @@ namespace Chords
             
                 if ("ChordIntent".Equals(intentName))
                 {
-                    return ProcessChord(intent);
+                    return new ChordProcessor().ProcessChord(intent);
                 }
 
                 if ("AMAZON.CancelIntent".Equals(intentName) || "AMAZON.StopIntent".Equals(intentName))
@@ -48,37 +47,6 @@ namespace Chords
             
             Trace.WriteLine("About to fail");
             throw new SpeechletException("Invalid Intent");
-        }
-
-        private static SpeechletResponse ProcessChord(Intent intent)
-        {
-            var chordName = intent.Slots["chord"];
-            Trace.WriteLine($"Chord was: {chordName.Value}");
-
-            if (string.IsNullOrEmpty(chordName.Value))
-            {
-                return PlainTextResponseFactory.Create("Didn't recognise that chord", false);
-            }
-            
-            try
-            {
-                return ProcessChord(chordName.Value);
-            }
-            catch (ChordNotFoundException exception)
-            {
-                var chord = string.Join(" ", exception.Words);
-                return PlainTextResponseFactory.Create($"Didn't recognise chord {chord}", false);
-            }
-        }
-        
-        private static SpeechletResponse ProcessChord(string chordName)
-        {
-            var chord = new ChordFinder().GetChord(chordName);
-
-            Trace.WriteLine($"Notes are : {string.Join(", ", chord.Notes)}");
-
-            var ssml = $"Notes in {chord.Name} <break strength='medium'/> are <break strength='medium'/> {chord.ToNotesSsml()}";
-            return SsmlResponseFactory.Create(ssml, false);
         }
 
         public override void OnSessionEnded(SessionEndedRequest sessionEndedRequest, Session session)
