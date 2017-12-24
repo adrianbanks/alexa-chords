c550310efa49f3e1af797335c2f1ca4ab8efaf8b
Adrian Banks <github@adrianbanks.co.uk>
2017-12-24 01:08:20 +0000

    Add tests for parsing the chord name into note and shape

---
+++ Chords.Tests/WordsSegragationTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Chords.Tests
+{
+    [TestFixture]
+    public sealed class WordsSegragationTests
+    {
+        [TestCase("C")]
+        [TestCase("F")]
+        [TestCase("G")]
+        public void HandlesSingleNoteChords(string chordName)
+        {
+            var segragation = new WordsSegragation(chordName);
+            
+            Assert.That(segragation.Note, Is.EqualTo(chordName).IgnoreCase);
+            Assert.That(segragation.Shape, Is.Empty);
+        }
+        
+        [TestCase("C minor", "C", "minor")]
+        [TestCase("F major", "F", "major")]
+        [TestCase("G seventh", "G", "seventh")]
+        public void HandlesWhiteNoteChords(string chordName, string expectedNote, string expectedShape)
+        {
+            var segragation = new WordsSegragation(chordName);
+            
+            Assert.That(segragation.Note, Is.EqualTo(expectedNote).IgnoreCase);
+            Assert.That(segragation.Shape, Is.EqualTo(expectedShape));
+        }
+        
+        [TestCase("C sharp minor", "Csharp", "minor")]
+        [TestCase("F sharp major", "Fsharp", "major")]
+        [TestCase("G flat seventh", "Gflat", "seventh")]
+        public void HandlesBlackNoteChords(string chordName, string expectedNote, string expectedShape)
+        {
+            var segragation = new WordsSegragation(chordName);
+            
+            Assert.That(segragation.Note, Is.EqualTo(expectedNote).IgnoreCase);
+            Assert.That(segragation.Shape, Is.EqualTo(expectedShape));
+        }
+    }
+}
